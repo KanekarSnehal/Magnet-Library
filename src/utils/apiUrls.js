50a1ca5783e333d8048dedc74ae469d1be6a288f
KanekarSnehal <snehalkanekar43@gmail.com>
2022-04-22 16:08:56 +0530

    added api urls for playlist

--- src/utils/apiUrls.js
+++ src/utils/apiUrls.js
@@ -2,3 +2,4 @@ export const videoUrl = "/api/videos";
 export const categoryUrl = "/api/categories";
 export const likedUrl = "/api/user/likes";
 export const watchLaterUrl = "/api/user/watchlater";
+export const playlistUrl = "/api/user/playlists";
