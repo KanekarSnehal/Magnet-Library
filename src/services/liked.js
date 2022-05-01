cc319c83cf21b103bb1a526657b09ac5b0446bf5
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-01 23:41:38 +0530

    added toast to liked

--- src/services/liked.js
+++ src/services/liked.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 import { likedUrl } from "../utils/index";
 import { likedConstants } from "../utils/dataActions";
+import { toast } from "react-toastify";
 
-const { ADD_TO_LIKED, REMOVE_FROM_LIKED, ERROR } = likedConstants;
+const { ADD_TO_LIKED, REMOVE_FROM_LIKED, GET_LIKED_VIDEOS } = likedConstants;
 
 const config = {
   headers: { authorization: localStorage.getItem("token") },
+};
+
+export const getLikedVideos = async (dispatch) => {
+  try {
+    const response = await axios.get(likedUrl, config);
+    if (response.status === 200) {
+      dispatch({ type: GET_LIKED_VIDEOS, payload: response.data.likes });
+    }
+  } catch (e) {
+    toast.error(e?.response?.data?.errors[0]);
+  }
 };
 
 export const addToLiked = async (video, dispatch) => {
@@ -14,9 +26,10 @@ export const addToLiked = async (video, dispatch) => {
 
     if (response.status === 201) {
       dispatch({ type: ADD_TO_LIKED, payload: response.data.likes });
+      toast.success("Video added to your liked videos!");
     }
   } catch (e) {
-    dispatch({ type: ERROR, payload: e });
+    toast.error(e?.response?.data?.errors[0]);
   }
 };
 
@@ -25,8 +38,9 @@ export const removeFromLiked = async (videoID, dispatch) => {
     const response = await axios.delete(`${likedUrl}/${videoID}`, config);
     if (response.status === 200) {
       dispatch({ type: REMOVE_FROM_LIKED, payload: response.data.likes });
+      toast.success("Video deleted from your liked videos!");
     }
   } catch (e) {
-    dispatch({ type: ERROR, payload: e });
+    toast.error(e?.response?.data?.errors[0]);
   }
 };
