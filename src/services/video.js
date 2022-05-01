4421f80cec4d0f7b4c3b67d7228ba37e1a28d6d6
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-01 23:42:02 +0530

    Added toast to video

--- src/services/video.js
+++ src/services/video.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 import { videoUrl } from "../utils/index";
 import { videoConstants } from "../utils/dataActions";
+import { toast } from "react-toastify";
 
 const { GET_VIDEOS, LOADING, ERROR, GET_VIDEO } = videoConstants;
 
 export const getVideos = async (dispatch) => {
   try {
-    dispatch({ type: LOADING });
     const response = await axios.get(videoUrl);
     if (response.status === 200) {
       dispatch({ type: GET_VIDEOS, payload: response.data.videos });
     }
   } catch (e) {
-    dispatch({ type: ERROR, payload: e });
+    toast.error(e?.response?.data?.errors[0]);
   }
 };
 
@@ -22,9 +22,9 @@ export const getSingleVideo = async (dispatch, videoID) => {
     const response = await axios.get(`/api/video/${videoID}`);
     if (response.status === 200) {
       dispatch({ type: GET_VIDEO, payload: response.data.video });
+      dispatch({ type: LOADING });
     }
   } catch (e) {
-    console.log(e);
-    dispatch({ type: ERROR, payload: e });
+    toast.error(e?.response?.data?.errors[0]);
   }
 };
