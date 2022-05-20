68e45c629103b9ed23f8d1fcb8d2e5219e6f81d3
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-21 05:18:34 +0530

    added profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import "./profile.css";
 import { useAuth } from "../../context/index";
 
 export const Profile = () => {
-  const { user, authDispatch } = useAuth();
+  const user = JSON.parse(localStorage.getItem("user"));
   const logoutHandler = (e) => {
     authDispatch({ type: "USER_LOGOUT" });
     localStorage.removeItem("token");
@@ -14,14 +14,22 @@ export const Profile = () => {
     <>
       <h1 className="text-center">Account</h1>
 
-      <div className="display-flex-row profile-container">
-        <div className="display-flex-row">
-          <p className="px-16"> Name</p>
-          <p>{user}</p>
-          <button className="btn primary-btn" onClick={logoutHandler}>
-            Logout
-          </button>
+      <div className="profile-container">
+        <div className="profile-container-row">
+          <p className="px-16 text-bold-weight">First Name</p>
+          <p>{user.firstName}</p>
         </div>
+        <div className="profile-container-row">
+          <p className="px-16 text-bold-weight">Last Name</p>
+          <p>{user.lastName}</p>
+        </div>
+        <div className="profile-container-row">
+          <p className="px-16 text-bold-weight">Email</p>
+          <p>{user.email}</p>
+        </div>
+        <button className="btn primary-btn" onClick={logoutHandler}>
+          Logout
+        </button>
       </div>
     </>
   );
